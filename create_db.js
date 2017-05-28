a1c0360d671e1d8cba43e1ee2bf2e0d904aff4bc
sin <nhat.nguyen@dinosys.vn>
2017-05-28 13:43:54 +0700

    edit comment

--- create_db.js
+++ create_db.js
@@ -6,16 +6,16 @@ const env = process.env.NODE_ENV;
  * @param {function} callback the function that will check error in terminal.
  */
 function checkEnv(callback) {
-    if(env == 'test') {
-      exec('createdb myblog-test', callback);
-    } else if(env == 'development') {
-      exec('createdb myblog-development', callback);
-    } else if(env == 'production') {
-      exec('createdb myblog-production', callback);
-    } else{
-      console.error('You should set environment first');
-      return;
-    }
+  if(env == 'test') {
+    exec('createdb myblog-test', callback);
+  } else if(env == 'development') {
+    exec('createdb myblog-development', callback);
+  } else if(env == 'production') {
+    exec('createdb myblog-production', callback);
+  } else{
+    console.error('You should set environment first');
+    return;
+  }
 };
 
 checkEnv((err, stdout, stderr) => {
